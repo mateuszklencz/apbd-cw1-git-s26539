656c3f55b8ccf92d315971430479856a0b3f8807
Mateusz Klencz <m.klencz20@gmail.com>
2026-05-30 19:06:09 +0200

    first basic input handling

--- apbd1/Program.cs
+++ apbd1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("Enter numbers in formatk like: 2, 3, 5 ");
+
+// One-liner to read, split, parse, and store into an array
+int[] numbers = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+// Verification (Just to prove it's in the array)
+Console.WriteLine($"Saved {numbers.Length} items. First item is: {numbers[0]}");
